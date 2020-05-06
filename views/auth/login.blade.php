3e3a408a2c11b02d1a19a9d0651660502e1db55e
sabrinadiza <60166748+sabrinadiza@users.noreply.github.com>
2020-05-06 20:28:57 +0700

    Update login.blade.php

--- views/auth/login.blade.php
+++ views/auth/login.blade.php
@@ -4,286 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content = "width=device-width, initial-scale=1.0">
         <meta http.equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/login-style.css')}}">
         <title></title>
 </head>
 <style type="text/css">
-  html, body{
-    height: 100%;
-}
-
-body{
-    margin: 0;
-    background-color: white;
-    font-family: Gilroy;
-    font-style: normal;
-}
-
-a{
-    text-decoration: none;
-    color: #000000;  
-}
-
-input{
-    font-family: "Gilroy";
-    font-weight: 800;
-    font-size: 18px;
-    line-height: 22px;
-    display: flex;
-    align-items: center;
-    letter-spacing: 0.1em;
-    color: #F7941E;
-}
-
-.wrapper{
-    width: 100%;
-    max-width: 1440px;
-    margin: 0 auto;
-}
-
-.tab{ 
-    position: absolute;
-    width: 1440px;
-    height: 116px;
-    left: 0px;
-    top: 0px;
-}
-
-.kotak_tab{
-    width: 1440px;
-    height: 116px;
-    background: #FAFAFAFA;
-}
-
-.menu{
-    position: relative;
-    width: 455.87px;
-    height: 28.63px;
-    left: 492px;
-    top: 44px;
-    display: flex;
-    justify-content:space-between;
-}
-
-.tulisan_tab{
-    margin-left: 486px;
-    margin-top: 49.535px;
-    width: 468px;
-    display: flex;
-    justify-content:space-between;
-}
-
-.logo{
-    position: relative;
-    width: 76px;
-    height: 73.29px;
-    left: 75px;
-    top: -8px;
-}
-
-.login{
-    position: absolute;
-    width: 1134px;
-    height: 515px;
-    left: 151px;
-    top: 166px;
-}
-
-#judul_login{
-    position: absolute;
-    left: 0%;
-    right: 32.72%;
-    top: 0%;
-    bottom: 86.6%;
-    font-weight: 800;
-    font-size: 56px; 
-    color: #000000;
-}
-
-#subtitle_login{
-    position: absolute;
-    left: 0.09%;
-    right: 53.35%;
-    top: 14.37%;
-    bottom: 80.58%;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 140.62%;
-    color: #000000;
-}
-
-.vektor{
-    position: absolute;
-    left: 57.85%;
-    right: 0%;
-    top: 4.66%;
-    bottom: 1.17%;
-}
-
-.kotak_un{
-    position: absolute;
-    padding: 23px;
-    width: 529px;
-    height: 69px;
-    left: 0%;
-    right: 53.35%;
-    top: 32.23%;
-    bottom: 54.37%;
-    border: 3px solid #F7941E;
-    box-sizing: border-box;
-    border-radius: 5px;
-}
-
-.kotak_pass{
-    position: absolute;
-    padding: 23px;
-    width: 529px;
-    height: 69px;
-    left: 0%;
-    right: 53.35%;
-    top: 53.01%;
-    bottom: 33.59%;
-    border: 3px solid #F7941E;
-    box-sizing: border-box;
-    border-radius: 5px;
-}
-
-.submit-btn{
-    position: absolute;
-    width: 351px;
-    height: 69px;
-    left: 6.17%;
-    right: 62.87%;
-    top: 77.67%;
-    bottom: 8.93%;
-    background: #F7941E;
-    border-radius: 40px;
-}
-
-#create{
-    position: absolute;
-    left: 8.11%;
-    right: 79.01%;
-    top: 95.15%;
-    bottom: 0%;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 140.62%;
-    color: #000000;
-}
-
-#create:hover {
-    color:#F7941E;
-}
-
-#forget{
-    position: absolute;
-    left: 21.6%;
-    right: 65.34%;
-    top: 95.15%;
-    bottom: 0%;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 140.62%;
-    color: #000000;
-}
-
-#forget:hover {
-    color:#F7941E;
-}
-
-#menu:hover {
-    color:#F7941E;
-}
-
-#submit-btn {
-    font-weight: 800;
-    font-size: 24px;
-    line-height: 29px;
-    position: absolute;
-    width: 351px;
-    height: 69px;                    
-    left: 6.17%;
-    right: 62.87%;
-    top: 77.67%;
-    bottom: 8.93%;
-    background: #F7941E;
-    border-radius: 40px;
-    border: none;
-    cursor: pointer;
-    color: white;
-    font-family: "Gilroy";
-    margin: 0 auto;
-    transition: 0.25s;
-}
-#submit-btn:hover {
-    box-shadow: 0px 1px 18px #F7941E;
-}
-::-webkit-input-placeholder{
-    font-family: "Gilroy";
-    font-weight: 800;
-    font-size: 18px;
-    line-height: 22px;
-    display: flex;
-    align-items: center;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;
-    color: #F7941E;
-}
-
-.footer{
-    position: absolute;
-    width: 1440px;
-    height: 80px;
-    left: 0px;
-    top: 770px;
-    background-color: #231F20;
-}
-
-#privacy{
-    position: relative;
-    left: 75.56%;
-    right: 5.21%;
-    top: 41.25%;
-    bottom: 41.25%;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 140.62%;
-    text-align: center;
-    color: #FAFAFA;
-}
-
-#tentang{
-    position: absolute;                    
-    width: 51px;
-    height: 25px;
-    left: 75px;
-    top: 15px;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 140.62%;
-    color: #FAFAFA;
-}
-
-#follow{
-    position: absolute;
-    width: 76px;
-    height: 25px;
-    left: 75px;
-    top: 40px;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 140.62%;
-    color: #FAFAFA;
-}
-
-.sosmed{
-    position: absolute;
-    width: 92px;
-    height: 29px;
-    left: 158px;
-    top: 36px;
-    margin-left: 10px;
-}
+  
 </style>
 <body>
 <div class="font">
@@ -311,12 +36,11 @@ input{
                 </div>
 
                 @if(isset(Auth::user()->email))
-                  <script>window.location={{url('/login/successlogin')}}</script>
+                  <script>window.location={{url('/home')}}</script>
                 @endif
 
                 @if($message = Session::get('error'))
                 <div class="alert alert-danger alert-block">
-                  <button type="button" class="close" data-dismiss="alert">x</button>
                   <strong>{{$message}}</strong>
                 </div>
                 @endif
@@ -331,7 +55,7 @@ input{
                   </div>
                 @endif
 
-                <form method="POST" action="{{url('/login/checklogin')}}">
+                <form method="POST" action="{{route('login')}}">
                   {{csrf_field()}}
                     <input id="user-email" class="kotak_un" type="email" name="email" autocomplete="on" placeholder="EMAIL" required />
                     <input id="user-password" class="kotak_pass" type="password" name="password" placeholder="PASSWORD" required />
